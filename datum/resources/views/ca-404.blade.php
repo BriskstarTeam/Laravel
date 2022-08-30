664be391151e9b9c771da06cadf8b0046755dd56
BriskstarTeam <sales@briskstartech.com>
2022-08-30 18:01:26 +0530

    update project directory

--- datum/resources/views/ca-404.blade.php
+++
@@ -1,9 +0,0 @@
-<link rel="stylesheet" type="text/css" href="{{ asset('public/assets/css/error_ca.css') }}" lazyload="1">
-<section class="error-section pt-50" style="padding-top: unset;padding-bottom: unset;width: 100%; text-align: center;">
-    <picture>
-        <img src="https://datumdocstorage.blob.core.windows.net/datumfilecontainer/error/404-error.jpg" alt="Sketch of a man frustrated with his computer" class="img-responsive" loading="lazy" width="380px">
-    </picture>
-    <div class="error-text">
-        <p style="width:100%;">Confidentiality Agreement is not available.</p>
-    </div>
-</section>
